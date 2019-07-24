ec8886a80c4c75d958a8da6cdb139d7f4244b1ff
Peliukas <pavel.kocia@gmail.com>
2019-07-24 18:55:40 +0200

    Updated add and update model endpoints with tournament. Added model management for PageConfig. Added search by param name endpoint. Added endpoint for assigning fighter to a fightclub. Created endpoint for viewing tournament program.

--- src/routes.php
+++ src/routes.php
@@ -25,7 +25,6 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
 $container = $app->getContainer();
-// $container['upload_directory'] = __DIR__ . '/uploads';
 $container['upload_directory'] = '../public/images';
 
 
@@ -54,8 +53,67 @@ $app->post('/api/{model_name}', function (Request $request, Response $response, 
         $fightclub->phone = $request->getParsedBody()['phone'];
         $fightclubid = R::store($fightclub);
         return $response->getBody()->write($fightclubid);
+        case "tournament":
+        $tournament = R::dispense('tournament');
+        $tournament->name = $request->getParsedBody()['name'];
+        $tournament->startdate = $request->getParsedBody()['startdate'];
+        $tournament->enddate = $request->getParsedBody()['enddate'];
+        $tournament->city = $request->getParsedBody()['city'];
+        $tournament->address = $request->getParsedBody()['address'];
+        $tournament->description = $request->getParsedBody()['description'];
+        $tournamentid = R::store($tournament);
+        $tournamentFights = $request->getParsedBody()['tournament_fights'];
+        if($tournamentFights){
+            foreach($tournamentFights as $fight){
+                $tournamentFight = R::dispense('tournamentfight');
+                $tournamentFight->tournamentid = $tournamentid;
+                $tournamentFight->winnerfighterid = 0;
+                $tournamentfightid = R::store($tournamentFight);
+                
+                $fightContestant = R::dispense('fightcontestant');
+                $fightContestant->fighterid = $fight["red"]["id"];
+                $fightContestant->tournamentfightid = $tournamentfightid;
+                $fightContestant->score = 0;
+                R::store($fightContestant);
+                
+                $fightContestant = R::dispense('fightcontestant');
+                $fightContestant->fighterid = $fight["blue"]["id"];
+                $fightContestant->tournamentfightid = $tournamentfightid;
+                $fightContestant->score = 0;
+                R::store($fightContestant);
+            }
+        }
+        return $response->getBody()->write($tournamentid);
+        case "pageconfiguration":
+        $page_name = $request->getParsedBody()["page_name"];
+        $param_name = $request->getParsedBody()["param_name"];
+        $param_value = $request->getParsedBody()["param_value"];
+        $pageConfiguration = R::dispense('pageconfiguration');
+        $pageConfiguration->page = $page_name;
+        $pageConfiguration->paramname = $param_name;
+        $pageConfiguration->paramvalue = $param_value;
+        $pageConfigurationId = R::store($pageConfiguration);
+        return $response->getBody()->write($pageConfigurationId);
     }
 });
+
+$app->post('/api/update/pageconfig/{page_name}/', function (Request $request, Response $response, array $args) {
+    $page_name = $args["page_name"];
+    $page_configs = $request->getParsedBody()["page_configs"];
+    foreach($page_configs as $configname => $configvalue){
+        $pageConfiguration = R::find('PageConfiguration', ' pagename = ? AND paramname = ? ', array($page_name, $configname) );
+        if(!empty($pageConfiguration)){
+            $pageConfiguration->param_value = $configvalue;
+            R::store($pageConfiguration);
+        }else{
+            $pageConfiguration = R::dispense('PageConfiguration');
+            $pageConfiguration->paramname = $configname;
+            $pageConfiguration->paramvalue = $configvalue;
+            R::store($pageConfiguration);
+        }
+    }
+    
+});    
 
 $app->post('/api/update/{model_name}/{id}', function (Request $request, Response $response, array $args) {
     $model_name = $args['model_name'];
@@ -81,26 +139,69 @@ $app->post('/api/update/{model_name}/{id}', function (Request $request, Response
         $object->phone = $request->getParsedBody()['phone'];
         $fightclubid = R::store($object);
         return $response->getBody()->write($fightclubid);
+        case "tournament":
+        $object->name = $request->getParsedBody()['name'];
+        $object->startdate = $request->getParsedBody()['startdate'];
+        $object->enddate = $request->getParsedBody()['enddate'];
+        $object->city = $request->getParsedBody()['city'];
+        $object->address = $request->getParsedBody()['address'];
+        $object->description = $request->getParsedBody()['description'];
+        $tournamentid = R::store($object);
+        $tournamentFights = $request->getParsedBody()['tournament_fights'];
+        if($tournamentFights){
+            foreach($tournamentFights as $fight){
+                if(empty($fight["id"])){
+                    $tournamentFight = R::dispense('tournamentfight');
+                    $tournamentFight->tournamentid = $tournamentid;
+                    $tournamentFight->winnerfighterid = !empty($fight['winnerfighterid']) ? $fight['winnerfighterid'] : "" ;
+                    $tournamentFight->videourl = $fight['videourl'];
+                    $tournamentfightid = R::store($tournamentFight);
+                    
+                    $fightContestant = R::dispense('fightcontestant');
+                    $fightContestant->fighterid = $fight["red"]["id"];
+                    $fightContestant->tournamentfightid = $tournamentfightid;
+                    $fightContestant->score = 0;
+                    R::store($fightContestant);
+                    
+                    $fightContestant = R::dispense('fightcontestant');
+                    $fightContestant->fighterid = $fight["blue"]["id"];
+                    $fightContestant->tournamentfightid = $tournamentfightid;
+                    $fightContestant->score = 0;
+                    R::store($fightContestant);
+                }else{
+                    $tournamentFight = R::load('tournamentfight', $fight["id"]);
+                    if(empty($fight["deleted"])){
+                        $tournamentFight->winnerfighterid = !empty($fight['winnerfighterid']) ? $fight['winnerfighterid'] : "" ;
+                        $tournamentFight->videourl = $fight['videourl'];
+                        $tournamentfightid = R::store($tournamentFight);
+                        
+                        $fightContestant = R::load('fightcontestant', ' fighterid = ' . $fight["red"]["id"] . ' AND tournamentfightid = ' . $fight["id"]);
+                        $fightContestant->fighterid = $fight["red"]["id"];
+                        $fightContestant->score = 0;
+                        R::store($fightContestant);
+                        
+                        $fightContestant = R::load('fightcontestant', ' fighterid = ' . $fight["blue"]["id"] . ' AND tournamentfightid = ' . $fight["id"]);
+                        $fightContestant->fighterid = $fight["blue"]["id"];
+                        $fightContestant->score = 0;
+                        R::store($fightContestant);
+                    }else{
+                        $tournamentFightContestants = R::findAll('fightcontestant', ' tournamentfightid = ' . $fight["id"]);
+                        foreach($tournamentFightContestants as $fightContestant){
+                            R::trash('fightcontestant', R::load("fightcontestant", $fightContestant["id"]));
+                        }
+                        R::trash('tournamentfight', $tournamentFight);
+                    }
+                }
+            }
+        }
+        return $response->getBody()->write($tournamentid);
     }
     return false;
 });
 
-$app->post('/api/assign/fighters/{club_id}', function (Request $request, Response $response, array $args) {
-    $club_id = $args['club_id'];
-    $fighter_list = $request->getParsedBody()['fighterlist'];
-    foreach($fighter_list as $fighter_id ){
-        $fighter = R::load('fighter', $fighter_id);
-        $fighter->fightclubid = $club_id;
-        R::store($fighter);
-    }
-    return $response->getBody()->write(true);
-});
-
-$app->get('/api/search/{model_name}/{param_name}/{param_value}', function (Request $request, Response $response, array $args) {
-    $model_name = $args['model_name'];
-    $param_name = $args['param_name'];
-    $param_value = $args['param_value'];
-    $result = R::find($model_name, $param_name . ' LIKE ? ', array('%'.$param_value.'%'));
+$app->get('/api/pageconfiguration/{page_name}', function(Request $request, Response $response, array $args){
+    $page_name = $args['page_name'];
+    $result = R::find('pageconfiguration', ' page = ? ', array($page_name));
     return $response->getBody()->write(json_encode($result));
 });
 
@@ -114,12 +215,35 @@ $app->get('/api/clubmembers/{club_id}/{param_name}/{param_value}', function (Req
     return $response->getBody()->write(json_encode($result));
 });
 
-$app->get('/api/{model_name}', function (Request $request, Response $response, array $args) {
+$app->get('/api/search/{model_name}/{param_name}/{param_value}', function (Request $request, Response $response, array $args) {
     $model_name = $args['model_name'];
-    $result = R::findAll($model_name);
+    $param_name = $args['param_name'];
+    $param_value = $args['param_value'];
+    $result = R::find($model_name, $param_name . ' LIKE ? ', array('%'.$param_value.'%'));
     return $response->getBody()->write(json_encode($result));
 });
 
+// get all models
+$app->get('/api/{model_name}', function (Request $request, Response $response, array $args) {
+    $model_name = $args['model_name'];
+    $result = R::findAll($model_name);
+    switch($model_name){
+        case "fightclub":
+        $result = R::findAll($model_name);
+        foreach($result as $fightclub){
+            $fightclub->membercount = R::count('fighter', ' fightclubid = ' . $fightclub->id);
+        }
+        break;
+        case "tournament":
+        $result = R::findAll($model_name);
+        break;
+        default:
+        break;
+    }
+    return $response->getBody()->write(json_encode($result));
+});
+
+// club member endpoints
 $app->get('/api/clubmembers/{id}', function (Request $request, Response $response, array $args) {
     $id = $args['id'];
     $result = R::findAll('fighter', ' fightclubid = ?', array($id));
@@ -130,6 +254,38 @@ $app->get('/api/fighters/unassigned', function (Request $request, Response $resp
     $result = R::findAll('fighter', ' fightclubid = 0');
     return $response->getBody()->write(json_encode($result));
 });
+
+$app->post('/api/assign/fighters/{club_id}', function (Request $request, Response $response, array $args) {
+    $club_id = $args['club_id'];
+    $fighter_list = $request->getParsedBody()['fighterlist'];
+    foreach($fighter_list as $fighter_id ){
+        $fighter = R::load('fighter', $fighter_id);
+        $fighter->fightclubid = $club_id;
+        R::store($fighter);
+    }
+    return $response->getBody()->write(true);
+});
+
+
+
+$app->get('/api/tournamentprogram/{tournament_id}', function (Request $request, Response $response, array $args) {
+    $tournament_id = $args['tournament_id'];
+    $tournamentFights = R::findAll('tournamentfight', ' tournamentid = ' . $tournament_id);
+    $results = array();
+    foreach($tournamentFights as $fight){
+        $contestants = R::findAll('fightcontestant', 'tournamentfightid = ' . $fight['id']);
+        $fightContestants = [];
+        foreach($contestants as $contestant){
+            $contestant['fighter'] = R::load('fighter', $contestant->fighterid);
+            $fightContestants[] = $contestant;
+        }
+        $fight['fight_contestants'] = $fightContestants;
+        $results[] = $fight;
+    }
+    return $response->getBody()->write(json_encode($results));
+});
+
+
 
 $app->get('/api/fighters/unassigned/{param_name}/{param_value}', function (Request $request, Response $response, array $args) {
     $param_name = $args['param_name'];
@@ -145,13 +301,37 @@ $app->delete('/api/{model_name}/{id}', function (Request $request, Response $res
     $id = $args['id'];
     $object = R::load($model_name, $id);
     if(!empty($object)){
-        $result = R::trash($object);
-        return $response->getBody()->write(true);
+        switch($model_name){
+            case "fighter":
+            $result = R::trash($object);
+            return $response->getBody()->write(true);
+            case "fightclub":
+            $fightclubMembers = R::findAll("fighter", " fightclubid = " . $object->id);
+            foreach($fightclubMembers as $member){
+                $fightclubMember = R::load("fighter", $member->id);
+                $fightclubMember->fightclubid = 0;
+                R::store($fightclubMember);
+            }
+            $result = R::trash($object);
+            return $response->getBody()->write($result);
+            case "tournament":
+                $tournamentFights = R::findAll("tournamentfight", " tournamentid = " . $object->id);
+                foreach($tournamentFights as $fight){
+                    $fightContestants = R::findAll("fightcontestant", " tournamentfightid = " . $fight->id);
+                    foreach($fightContestants as $contestant){
+                        R::trash("fightcontestant", $contestant->id); 
+                    }
+                    R::trash("tournamentfight", $fight->id);
+                }
+                R::trash($model_name, $id);
+            return $response->getBody()->write(true);
+        }
     }
     return $response->getBody()->write(false);
 });
 
 
+// image upload
 $app->post('/api/{model_name}/image/{id}', function(Request $request, Response $response, array $args) {
     $model_name = $args['model_name'];
     $id = $args['id'];
